af5f98e246a6061bff9ad7753850af74e6c173cd
blam1998 <57881360+blam1998@users.noreply.github.com>
2023-10-10 13:28:40 -0700

    Testing GA 4.

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,13 +6,10 @@ import Header from '@/components/Header'
 import ActiveContext from '@/context/ActiveContext'
 import ReactGA from "react-ga4";
 
-try{
-  ReactGA.initialize("G-79QE0418LQ");
-  ReactGA.send({hitType: "pageview", page: window.location.pathname, title: "Home page"});
-}
-catch(error:any){
-  console.log(error);
-}
+
+ReactGA.initialize("G-79QE0418LQ");
+ReactGA.send({hitType: "pageview", page: window.location.pathname});
+
 
 
 const inter = Inter({ subsets: ['latin'] })
--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,8 @@ import Contact from '@/components/Contact'
 import RotateImage from '@/components/RotateImage'
 import ReactGA from "react-ga4";
 
-try{
-  ReactGA.initialize("G-79QE0418LQ");
-  ReactGA.send({hitType: "pageview", page: window.location.pathname, title: "Home page"});
-}
-catch(error:any){
-  console.log(error);
-}
+ReactGA.initialize("G-79QE0418LQ");
+ReactGA.send({hitType: "pageview", page: window.location.pathname});
 
 export default function Home() {
   return (
